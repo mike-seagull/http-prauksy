d3b451c6561751d74494fc4120b2e04d8c91ec8d
Michael Hollister <hollister.michael@gmail.com>
2018-05-03 13:10:20 -0500

    locking down from GCP

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var proxy = require('http-proxy-middleware');
-
+var morgan = require('morgan');
 // proxy options
 var home_api_opts = {
 	target: 'http://127.0.0.1:3001', // target host
 	changeOrigin: true // needed for virtual hosted sites
 };
 
+var deploy_opts = {
+	target: 'http://127.0.0.1:3002', // target host
+	changeOrigin: true // needed for virtual hosted sites
+};
 // create the proxy (without context)
 var home_api = proxy(home_api_opts);
+var deploy = proxy(deploy_opts);
 
 var app = express();
+app.use(morgan('common'));
 app.use('/api', home_api);
+app.use('/deploy', deploy);
 app.listen(3000);
