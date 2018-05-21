f5b646d1d8f4706781dc998b2723f7567dfa7bed
Michael Hollister <hollister.michael@gmail.com>
2018-05-21 10:05:02 -0500

    lockdown

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var proxy = require('http-proxy-middleware');
 var morgan = require('morgan');
+var fs = require('fs')
+
 // proxy options
 var home_api_opts = {
 	target: 'http://127.0.0.1:3001', // target host
@@ -15,8 +17,20 @@ var deploy_opts = {
 var home_api = proxy(home_api_opts);
 var deploy = proxy(deploy_opts);
 
+function saveIp(req, resp, next) {
+	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	fs.appendFile('/home/michaelhollister/repos/http-proxy/access.log', ip+"\n", (err) => {
+		if (err) {
+			console.error(err);
+		}
+	});
+	next();
+}
+
+
 var app = express();
 app.use(morgan('common'));
+app.use(saveIp);
 app.use('/api', home_api);
 app.use('/deploy', deploy);
 app.listen(3000);
