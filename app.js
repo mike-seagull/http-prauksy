e94535d155b3beb4b8c6a5f9d1912958119e245e
Michael Hollister <hollister.michael@gmail.com>
2018-05-22 21:40:40 -0500

    only authorizing specific ips

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const https = require('https');
 const fs = require('fs');
 var proxy = require('http-proxy-middleware');
 var morgan = require('morgan');
+const ipRangeCheck = require("ip-range-check");
 
 // proxy options
 var home_api_opts = {
@@ -20,12 +21,36 @@ var deploy = proxy(deploy_opts);
 
 function saveIp(req, resp, next) {
 	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-	fs.appendFile('/home/michaelhollister/repos/http-proxy/access.log', ip+"\n", (err) => {
+	fs.appendFile('/home/michaelhollister/repos/http-proxy/logs/access.log', ip+"\n", (err) => {
 		if (err) {
 			console.error(err);
 		}
 	});
 	next();
+}
+
+function checkAuthorizedIp(req, resp, next) {
+	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	if (ip.substr(0, 7) == "::ffff:") {
+  		ip = ip.substr(7)
+	}
+	const authorized_ips = [
+		"12.250.0.0/16", // work
+		"173.12.0.0/16", // work
+		"76.107.0.0/16", // home
+		"104.196.32.52", // gcp
+		"127.0.0.1" // loopback
+	];
+	if (ipRangeCheck(ip, authorized_ips)) {
+		next();
+	} else {
+		fs.appendFile('/home/michaelhollister/repos/http-proxy/logs/blocked_access.log', ip+"\n", (err) => {
+			if (err) {
+				console.error(err);
+			}
+		});
+		resp.end();
+	}
 }
 
 const ssl_options = {
@@ -36,6 +61,7 @@ const ssl_options = {
 var app = express();
 app.use(morgan('common'));
 app.use(saveIp);
+app.use(checkAuthorizedIp);
 app.use(express.static(__dirname+'/static'));
 app.use('/api', home_api);
 app.use('/deploy', deploy);
