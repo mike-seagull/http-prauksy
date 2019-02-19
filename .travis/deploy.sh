23b326284e5e060afd106151af4f6470a433c4eb
Mike Hollister <hollister.michael@gmail.com>
2019-02-19 11:29:17 -0600

    removed deployment files

--- .travis/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# stop the service
-ssh -o LogLevel=quiet ${REMOTE_USER}@${REMOTE_SERVER} "/usr/bin/sudo systemctl stop http-prauksy"
-# scp the binary
-scp -o LogLevel=quiet ./bin/http-prauksy ${REMOTE_USER}@${REMOTE_SERVER}:/usr/local/bin/http-prauksy
-ssh -o LogLevel=quiet ${REMOTE_USER}@${REMOTE_SERVER} "/bin/chmod +x /usr/local/bin/http-prauksy"
-# start the service
-ssh -o LogLevel=quiet ${REMOTE_USER}@${REMOTE_SERVER} "/usr/bin/sudo systemctl start http-prauksy"
